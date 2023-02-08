c0485810e9e767fb239d2488e974c36cbd4222e8
kashifjaved1 <53248414+kashifjaved1@users.noreply.github.com>
2023-02-09 01:57:33 +0500

    Complete code.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Threading;
+
+namespace StringWordReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "This is C# program for reversing given string's each word";
+            int counter = 0;
+            ArrayList arylist = new ArrayList();
+            //char[] symbols = { ',', '\'', '!', '?', '.' };
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != ' ')// || !str[i].Equals(symbols.Any()))
+                {
+                    counter++;
+                }
+
+                if (str[i] == ' ')
+                {
+                    Reverse(str, i, counter, arylist);
+                    counter = 0;
+                }
+
+                if (i == str.Length-1)
+                {
+                    Reverse(str, i+1, counter, arylist);
+                }
+            }
+
+            foreach (var item in arylist)
+            {
+                Console.Write(item);
+            }
+
+            Console.WriteLine("");
+        }
+
+        public static int Reverse(string str, int index, int loopCounter, ArrayList arylist)
+        {
+            index = index - 1;
+            for (int i = 0; i <= loopCounter; i++)
+            {
+                if(i < loopCounter)
+                {
+                    arylist.Add(str[index]);
+                    index--;
+                }
+                if (i == loopCounter) arylist.Add(" ");
+            }
+            return 0;
+        }
+    }
+}
